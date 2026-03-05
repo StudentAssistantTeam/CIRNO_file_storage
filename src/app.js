be8fbe60ffae2e95fa4df6d47dc0b1c0175fd05e
NewtonZhan <zhanyunze0601@gmail.com>
2026-03-05 11:47:22 +0800

    feat(server): Adding starter for the server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-const config = require('./config');
+// logging
+var morgan = require('morgan');
+// Environment configuration
+var config = require('./config');
+// Server APP
+var express = require('express');
+var app = express();
+
+// Run app
+var server = app.listen(config.server_port, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Server running at http://%s:%s', host, port);
+});
