c8a61bb12c8b6b5a6db8e1004338f7372bc25042
NewtonZhan <zhanyunze0601@gmail.com>
2026-03-20 14:39:35 +0800

    feat(oss): Add file uploading endpoint

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 // Environment configuration
 var config = require('./config');
 
+// fs
+var fs = require('fs');
+
 // Server APP
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
-app.use(express.json())
+app.use(express.json());
+
+// Multer
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
 
 // OSS Manager
 var oss_manager = require('./oss_manager');
@@ -223,14 +230,14 @@ app.post('/listFiles',
             console.log(`List files in directory ${req.body.directory}`);
             const files = await oss_manager.listFiles(req.body.directory);
             var content = {
-                "success": true,
-                "files": files
+                success: true,
+                files: files
             };
             return res.status(200).json(content);
         }else{
             var content = {
-                "success": false,
-                "reason": "Directory not exist"
+                success: false,
+                reason: "Directory not exist"
             };
             return res.status(404).json(content);
         }
@@ -238,12 +245,86 @@ app.post('/listFiles',
         // Error handling
         console.error(err);
         var content = {
-            "success": false,
-            "reason": err
+            success: false,
+            reason: err
         };
         return res.status(500).json(content);
     }
 });
+
+/**
+ * @swagger
+ * /uploadFile:
+ *    post:
+ *      tags:
+ *      - Upload File
+ *      summary: Upload a file to a directory in OSS
+ *      description: Upload a file to a directory in OSS
+ *      produces:
+ *      - application/json
+ *      parameters:
+ *      - in: body
+ *        name: body
+ *        description: parameters for checking whether a file exists in OSS
+ *        required: true
+ *        schema:
+ *          type: object
+ *          properties:
+ *            directory:
+ *              type: string
+ *              description: directory name
+ *              example: my-directory
+ *            file:
+ *              type: string
+ *              format: binary
+ *              description: file to be uploaded
+ *      responses:
+ *        200:
+ *          description: successful operation
+ *        400:
+ *          description: Invalid input supplied
+ *        500:
+ *          description: Internal Server Error
+ * */
+// File Upload
+app.post('/uploadFile', 
+    upload.single('file'), 
+    body('directory').notEmpty().withMessage('Directory is required'),
+    body('file').notEmpty().withMessage('File is required'),
+    async function(req, res) {
+        try{
+            // Get file info
+            var file = req.file;
+            if(await oss_manager.fileExists(`${req.body.directory}/${file.filename}`)){
+                // Upload it to OSS
+                var result = await oss_manager.uploadFlieFromLocal(req.body.directory, file.path);
+                var content = {
+                    success: true,
+                    result: result
+                }
+                // Delete local file
+                fs.unlinkSync(file.path);
+                return res.status(200).json(content);
+            }else{
+                var content = {
+                    success: false,
+                    reason: "File already exist"
+                }
+                // Delete local file
+                fs.unlinkSync(file.path);
+                return res.status(409).json(content);
+            }
+        }catch(err){
+            // Error handling
+            console.error(err);
+            var content = {
+                success: false,
+                reason: err
+            }
+            return res.status(500).json(content);
+        }
+    }
+);
 
 // Run app
 var server = app.listen(config.server_port, function() {
